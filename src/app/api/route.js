81c3e29e527d7102e3e30f130ca31abf39123a4c
Erdenemunkh Tsenkherbor <ts.erka@gmail.com>
2024-03-26 00:15:47 +0800

    test

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,10 +1,13 @@
 import dayjs from "dayjs"
+import { NextResponse } from "next/server";
 
 export async function GET() {
   const date1 = new Date();
   const date2 = dayjs();
+  const res = await fetch('https://worldtimeapi.org/api/timezone/Asia/Ulaanbaatar')
+  const json = await res.json()
 
-  return Response.json({
-    date1, date2
+  return NextResponse.json({
+    date1, date2, json
   })
 }
